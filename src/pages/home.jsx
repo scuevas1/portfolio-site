ef07b168cc3aa6df2ac321329c0ac3621fd76de3
scuevas1 <scuevas1@hawk.iit.edu>
2025-11-18 10:55:28 -0600

    fix: adding content to home page.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,6 +6,7 @@ export default function Home(){
     return(
         <div className="index-page">
             <h1>Home Page</h1>
+            <p>Rafael smells really really really bad</p>
         </div>
     )
 }
